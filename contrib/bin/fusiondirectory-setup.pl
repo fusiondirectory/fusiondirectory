6ef0ec30d74beb746625137b3d1f85665d7e30e1
Benoit Mortier <benoit.mortier@opensides.be>
2011-08-19 18:30:51 +0200

    Removed not use state var and added copyright header

--- contrib/bin/fusiondirectory-setup.pl
+++ contrib/bin/fusiondirectory-setup.pl
@@ -1,4 +1,23 @@
 #!/usr/bin/perl
+#
+#
+#  This code is part of FusionDirectory (http://www.fusiondirectory.org/)
+#  Copyright (C) 2011  FusionDirectory
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 
 use strict;
 use warnings;
@@ -33,7 +52,6 @@ switch( $distro ) {
  
 
 my $locale_dir = $fd_home."locale";
-my $state_dir = $fd_home."state";
 my $class_cache = $fd_cache."class.cache";
 my $locale_cache_dir = $fd_cache."locale";
 
