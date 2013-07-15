710e05f72ffc99810ba7bb752bd8d2dbaf9775f3
Côme Bernigaud <come.bernigaud@laposte.net>
2013-07-15 15:06:59 +0200

    Fixes: #2546 Made FusionDirectory RightToLeft compliant

--- html/main.php
+++ html/main.php
@@ -114,7 +114,7 @@ if (!session::global_is_set('CurrentMainBase')) {
 putenv("LANGUAGE=");
 putenv("LANG=$lang");
 setlocale(LC_ALL, $lang);
-$GLOBALS['t_language']= $lang;
+$GLOBALS['t_language']            = $lang;
 $GLOBALS['t_gettext_message_dir'] = $BASE_DIR.'/locale/';
 
 /* Set the text domain as 'fusiondirectory' */
@@ -298,7 +298,8 @@ if (isset($_GET['reset'])) {
 /* show web frontend */
 $smarty->assign ("date", date("l, dS F Y H:i:s O"));
 $smarty->assign ("lang", preg_replace('/_.*$/', '', $lang));
-$smarty->assign ("must", "<font class=\"must\">*</font>");
+$smarty->assign ("rtl", language_is_rtl($lang));
+$smarty->assign ("must", '<span class="must">*</span>');
 if (isset($plug)) {
   $plug= "?plug=$plug";
 } else {
--- ihtml/themes/default/headers.tpl
+++ ihtml/themes/default/headers.tpl
@@ -1,5 +1,5 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" {if $rtl}dir="rtl" class="rtl"{else}class="ltr"{/if}>
 
 <head>
   <title>{if isset($title)}{$title}{else}FusionDirectory{/if}</title>
