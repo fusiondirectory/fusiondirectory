7b9c2445ddff69ae260f624863413984e34e62a5
Benoit Mortier <benoit.mortier@opensides.be>
2015-11-05 21:03:57 +0100

    Removing bad caracters again :/
    Signed-off-by: Benoit Mortier <benoit.mortier@opensides.be>

--- html/index.php
+++ html/index.php
@@ -238,7 +238,7 @@ class Index {
     $str = check_schema($cfg);
     foreach ($str as $tr) {
       if (isset($tr['IS_MUST_HAVE']) && !$tr['STATUS']) {
-        return _('LDAP schema check reported errors:').'<br/><br/><i>'.$tr['MSG'].'</i>';
+        return _('LDAP schema check reported errors:').'<br/><br/><i>'.$tr['MSG'].'</i>';
       }
     }
     return TRUE;
