5fa7eeea4d6712b13cf6d315c9963f5f9332ad0e
Côme Chilliet <come.bernigaud@opensides.be>
2016-06-21 06:23:42 +0200

    Fixes #4882 Putting 0 in session lifetime disables it

--- html/main.php
+++ html/main.php
@@ -76,17 +76,19 @@ if (session::global_get('_LAST_PAGE_REQUEST') != '') {
   /* check FusionDirectory.conf for defined session lifetime */
   $max_life = $config->get_cfg_value('sessionLifetime', 60 * 60 * 2);
 
-  /* get time difference between last page reload */
-  $request_time = (time() - session::global_get('_LAST_PAGE_REQUEST'));
+  if ($max_life > 0) {
+    /* get time difference between last page reload */
+    $request_time = (time() - session::global_get('_LAST_PAGE_REQUEST'));
 
-  /* If page wasn't reloaded for more than max_life seconds
-   * kill session
-   */
-  if ($request_time > $max_life) {
-    session::destroy();
-    logging::log('security', 'login', '', array(), 'main.php called with expired session - logging out');
-    header ('Location: index.php?signout=1&message=expired');
-    exit;
+    /* If page wasn't reloaded for more than max_life seconds
+     * kill session
+     */
+    if ($request_time > $max_life) {
+      session::destroy();
+      logging::log('security', 'login', '', array(), 'main.php called with expired session - logging out');
+      header ('Location: index.php?signout=1&message=expired');
+      exit;
+    }
   }
 }
 session::global_set('_LAST_PAGE_REQUEST', time());
