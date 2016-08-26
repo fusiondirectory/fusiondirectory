e79352d54be7cceb756876f60a171977a96f26fc
Côme Chilliet <come@opensides.be>
2016-08-26 16:42:20 +0200

    More javascript cleanups

--- html/include/pwdStrength.js
+++ html/include/pwdStrength.js
@@ -1,7 +1,7 @@
 /*************************************************************
 Created: 20060120
 Author:  Steve Moitozo <god at zilla dot us>
-Description: This is a quick and dirty password quality meter 
+Description: This is a quick and dirty password quality meter
      written in JavaScript
 License: MIT License (see below)
 =================================
@@ -45,117 +45,93 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 OR OTHER DEALINGS IN THE SOFTWARE.
 ---------------------------------------------------------------
- 
+
 ************************************************************ */
 function testPasswordCss(passwd)
 {
-    var intScore   = 0
-    
-    // PASSWORD LENGTH
-    if (passwd.length==0 || !passwd.length)                         // length 0
-    {
-      intScore = -1
-    }
-    else if (passwd.length>0 && passwd.length<5) // length between 1 and 4
-    {
-      intScore = (intScore+3)
-    }
-    else if (passwd.length>4 && passwd.length<8) // length between 5 and 7
-    {
-      intScore = (intScore+6)
-    }
-    else if (passwd.length>7 && passwd.length<12)// length between 8 and 15
-    {
-      intScore = (intScore+12)
-    }
-    else if (passwd.length>11)                    // length 16 or more
-    {
-      intScore = (intScore+18)
-    }
-    
-    
-    // LETTERS (Not exactly implemented as dictacted above because of my limited understanding of Regex)
-    if (passwd.match(/[a-z]/))                              // [verified] at least one lower case letter
-    {
-      intScore = (intScore+1)
-    }
-    
-    if (passwd.match(/[A-Z]/))                              // [verified] at least one upper case letter
-    {
-      intScore = (intScore+5)
-    }
-    
-    // NUMBERS
-    if (passwd.match(/\d+/))                                 // [verified] at least one number
-    {
-      intScore = (intScore+5)
-    }
-    
-    if (passwd.match(/(.*[0-9].*[0-9].*[0-9])/))             // [verified] at least three numbers
-    {
-      intScore = (intScore+5)
-    }
-    
-    
-    // SPECIAL CHAR
-    if (passwd.match(/.[!,@,#,$,%,^,&,*,?,_,~]/))            // [verified] at least one special character
-    {
-      intScore = (intScore+5)
-    }
-    
-                                 // [verified] at least two special characters
-    if (passwd.match(/(.*[!,@,#,$,%,^,&,*,?,_,~].*[!,@,#,$,%,^,&,*,?,_,~])/))
-    {
-      intScore = (intScore+5)
-    }
-  
-    
-    // COMBOS
-    if (passwd.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/))        // [verified] both upper and lower case
-    {
-      intScore = (intScore+2)
-    }
+  var intScore = 0
 
-          if (passwd.match(/([a-zA-Z])/) && passwd.match(/([0-9])/)) // [verified] both letters and numbers
-          {
-                  intScore = (intScore+2)
-          }
+  // PASSWORD LENGTH
+  if ((passwd.length === 0) || !passwd.length) {
+    intScore = -1
+  } else if (passwd.length > 11) {
+    intScore = (intScore+18)
+  } else if (passwd.length > 7) {
+    intScore = (intScore+12)
+  } else if (passwd.length > 4) {
+    intScore = (intScore+6)
+  } else {
+    intScore = (intScore+3)
+  }
 
-    // [verified] letters, numbers, and special characters
-    if (passwd.match(/([a-zA-Z0-9].*[!,@,#,$,%,^,&,*,?,_,~])|([!,@,#,$,%,^,&,*,?,_,~].*[a-zA-Z0-9])/))
-    {
-      intScore = (intScore+2)
-    }
 
-  
-    if(intScore == -1)
-    {
-       $('meterEmpty').setStyle({width: '100%'});
-         $('meterFull').setStyle({width: '0%'});
-    }
-    else if(intScore > -1 && intScore < 16)
-    {
-       $('meterEmpty').setStyle({width: '100%'});
-         $('meterFull').setStyle({width: '0%'});
-    }
-    else if (intScore > 15 && intScore < 25)
-    {
-       $('meterEmpty').setStyle({width: '100%'});
-         $('meterFull').setStyle({width: '25%'});
-    }
-    else if (intScore > 24 && intScore < 35)
-    {
-       $('meterEmpty').setStyle({width: '100%'});
-         $('meterFull').setStyle({width: '50%'});
-    }
-    else if (intScore > 34 && intScore < 45)
-    {
-       $('meterEmpty').setStyle({width: '100%'});
-         $('meterFull').setStyle({width: '75%'});
-    }
-    else
-    {
-       $('meterEmpty').setStyle({width: '100%'});
-         $('meterFull').setStyle({width: '100%'});
-    }
+  // LETTERS (Not exactly implemented as dictacted above because of my limited understanding of Regex)
+  if (passwd.match(/[a-z]/))                              // [verified] at least one lower case letter
+  {
+    intScore = (intScore+1)
+  }
+
+  if (passwd.match(/[A-Z]/))                              // [verified] at least one upper case letter
+  {
+    intScore = (intScore+5)
+  }
+
+  // NUMBERS
+  if (passwd.match(/\d+/))                                 // [verified] at least one number
+  {
+    intScore = (intScore+5)
+  }
+
+  if (passwd.match(/(.*[0-9].*[0-9].*[0-9])/))             // [verified] at least three numbers
+  {
+    intScore = (intScore+5)
+  }
+
+
+  // SPECIAL CHAR
+  if (passwd.match(/.[!,@,#,$,%,^,&,*,?,_,~]/))            // [verified] at least one special character
+  {
+    intScore = (intScore+5)
+  }
+
+  // [verified] at least two special characters
+  if (passwd.match(/(.*[!,@,#,$,%,^,&,*,?,_,~].*[!,@,#,$,%,^,&,*,?,_,~])/))
+  {
+    intScore = (intScore+5)
+  }
+
+
+  // COMBOS
+  if (passwd.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/))        // [verified] both upper and lower case
+  {
+    intScore = (intScore+2)
+  }
+
+  if (passwd.match(/([a-zA-Z])/) && passwd.match(/([0-9])/)) // [verified] both letters and numbers
+  {
+    intScore = (intScore+2)
+  }
+
+  // [verified] letters, numbers, and special characters
+  if (passwd.match(/([a-zA-Z0-9].*[!,@,#,$,%,^,&,*,?,_,~])|([!,@,#,$,%,^,&,*,?,_,~].*[a-zA-Z0-9])/)) {
+    intScore = (intScore+2)
+  }
+
+
+  if(intScore > -2 && intScore < 16) {
+    $('meterEmpty').setStyle({width: '100%'});
+    $('meterFull').setStyle({width: '0%'});
+  } else if (intScore > 15 && intScore < 25) {
+    $('meterEmpty').setStyle({width: '100%'});
+    $('meterFull').setStyle({width: '25%'});
+  } else if (intScore > 24 && intScore < 35) {
+    $('meterEmpty').setStyle({width: '100%'});
+    $('meterFull').setStyle({width: '50%'});
+  } else if (intScore > 34 && intScore < 45) {
+    $('meterEmpty').setStyle({width: '100%'});
+    $('meterFull').setStyle({width: '75%'});
+  } else {
+    $('meterEmpty').setStyle({width: '100%'});
+    $('meterFull').setStyle({width: '100%'});
+  }
 }
