4e5871a49ebcea90cb4f99280d63407960f9df52
Benoit Mortier <benoit.mortier@opensides.be>
2016-11-08 16:38:35 +0100

    Cleaning minor sonar code compliance Move this trailing comment on the previous empty line
    Signed-off-by: Benoit Mortier <benoit.mortier@opensides.be>

--- contrib/smarty/plugins/function.msgPool.php
+++ contrib/smarty/plugins/function.msgPool.php
@@ -20,20 +20,20 @@
 */
 function smarty_function_msgPool($params, &$smarty)
 {
-	if(class_available("msgPool") && isset($params['type'])){
-		$parameter = array();
-		foreach($params as $para => $value){
-			if(!preg_match("/^type$/i",$para)){
-				$parameter[$para] = $value;
-			}
-		}
-		if(is_callable("msgPool::".$params['type'])){
-			echo call_user_func_array(array("msgPool",$params['type']), $parameter);
-		}else{
-			trigger_error("Unknown msgPool function ".$params['type']);
-		}
-	}else{
-		trigger_error("Unknown class msgPool.");
-	}
+  if(class_available("msgPool") && isset($params['type'])){
+    $parameter = array();
+    foreach($params as $para => $value){
+      if(!preg_match("/^type$/i",$para)){
+        $parameter[$para] = $value;
+      }
+    }
+    if(is_callable("msgPool::".$params['type'])){
+      echo call_user_func_array(array("msgPool",$params['type']), $parameter);
+    }else{
+      trigger_error("Unknown msgPool function ".$params['type']);
+    }
+  }else{
+    trigger_error("Unknown class msgPool.");
+  }
 }
 ?>
